06b0b8f09db3d49b9fae4152fa5ac70e9e807169
Omkar Kumbhar <83999016+OmkarKumbhar2001@users.noreply.github.com>
2023-02-23 16:45:29 +0530

    skip

--- day 18/main.py
+++ day 18/main.py
@@ -1,18 +1,23 @@
 from turtle import Turtle,Screen
 import heroes
-print(heroes.gen())
 #from turtle import *#* means =>import everything in that  module
 # import turtle as t
 # timmy=t.Turtle()
 timmy=Turtle()
 timmy.shape("circle")
 timmy.pensize(10)
-# timmy.circle(80)
-timmy.color('red')
+timmy.circle(80)
+timmy.pen(fillcolor="violet", pencolor="blue", pensize=2)
+
 timmy.shape('turtle')
+timmy.forward(10)
+timmy.penup()
 for i in range(0,4):
-    timmy.forward(200)
-    timmy.dot("green")
+    for i in range(0,10):
+        timmy.pendown()
+        timmy.forward(10)
+        timmy.penup()
+        timmy.forward(10)
     timmy.left(90)
 
     
