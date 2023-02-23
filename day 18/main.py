795b47601310bad2a44b25a2e637a44c2a43749f
Omkar Kumbhar <83999016+OmkarKumbhar2001@users.noreply.github.com>
2023-02-23 11:46:46 +0530

    Add files via upload

---
+++ day 18/main.py
@@ -0,0 +1,32 @@
+from turtle import Turtle,Screen
+#from turtle import *#* means =>import everything in that  module
+# import turtle as t
+# timmy=t.Turtle()
+timmy=Turtle()
+timmy.shape("circle")
+timmy.pensize(10)
+# timmy.circle(80)
+timmy.color('red')
+timmy.shape('turtle')
+for i in range(0,4):
+    timmy.forward(200)
+    timmy.dot("green")
+    timmy.left(90)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen= Screen()
+screen.exitonclick()
