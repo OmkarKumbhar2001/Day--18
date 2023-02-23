d9d11a55715a82705ce56fdd1a16f70498bbffb6
Omkar Kumbhar <83999016+OmkarKumbhar2001@users.noreply.github.com>
2023-02-23 13:19:08 +0530

    update pattern.py with functions

--- day 18/pattern.py
+++ day 18/pattern.py
@@ -1,18 +1,23 @@
 from turtle import Turtle,Screen
+import random
 timmy=Turtle()
 timmy.pen(fillcolor="violet", pencolor="blue", pensize=2)
 timmy.shape('turtle')
 count=3
-current_angle=360
-colors=['red','blue','green','yellow','violet','orange','pink']
-for i in range(0,6):
-    current_angle=current_angle/count
+
+colors=['red','blue','green','yellow','violet','orange','pink','black']
+def dra_shape(count,current_angle):
+    timmy.pencolor(random.choice(colors))
     for j in range(0,count):
-        timmy.pencolor(colors[i])
         timmy.forward(100)
         timmy.right(current_angle)
+
+
+for i in range(0,10):
+    current_angle=360/count
+    dra_shape(count,current_angle)
     count+=1
-    current_angle=360
+
 
 screen= Screen()
 screen.exitonclick()
